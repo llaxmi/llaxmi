fb312db2a2405b18a0de93d5c71b60db46cd5980
ll4xmi <ll4xmi@gmail.com>
2025-01-05 22:43:49 +0545

    update mail address

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
 export const GITHUB_URL = "https://github.com/llaxmi";
 export const LINKEDIN_URL =
   "https://www.linkedin.com/in/laxmi-lamichhane-15a27630a/";
-export const MAIL = "ll4xmi@gmail.com";
+export const MAIL = "lc.llaxmi@gmail.com";
 export const X = "https://x.com/ll4xmii";
